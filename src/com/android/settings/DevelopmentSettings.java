0c7e3883dc809f9a8ab91b8ad354d349f5ad5aa6
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-09-14 04:03:18 +0100

    Development Settings: lets not hide chamber of secrets
    just looks weird

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -162,10 +162,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
 
-    private static final String KEY_CHAMBER_OF_SECRETS = "chamber_of_secrets";
-    private static final String KEY_CHAMBER_OF_UNLOCKED_SECRETS =
-            "chamber_of_unlocked_secrets";
-
     private static final int RESULT_DEBUG_APP = 1000;
 
     private static final String DEVELOPMENT_SHORTCUT_KEY = "development_shortcut";
@@ -228,9 +224,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private CheckBoxPreference mAdvancedReboot;
 
     private CheckBoxPreference mDevelopmentShortcut;
-
-    private Preference mChamber;
-    private CheckBoxPreference mChamberUnlocked;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
     private final ArrayList<CheckBoxPreference> mResetCbPrefs
@@ -367,23 +360,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         if (hdcpChecking != null) {
             mAllPrefs.add(hdcpChecking);
             removePreferenceForProduction(hdcpChecking);
-        }
-
-        mChamber = (Preference) findPreference(KEY_CHAMBER_OF_SECRETS);
-        mAllPrefs.add(mChamber);
-        mChamberUnlocked =
-                findAndInitCheckboxPref(KEY_CHAMBER_OF_UNLOCKED_SECRETS);
-        mChamberUnlocked.setOnPreferenceChangeListener(this);
-
-        boolean chamberOpened = Settings.Secure.getInt(
-                getActivity().getContentResolver(),
-                Settings.Secure.CHAMBER_OF_SECRETS, 0) == 1;
-        mChamberUnlocked.setChecked(chamberOpened);
-
-        if (chamberOpened) {
-            removePreference(mChamber);
-        } else {
-            removePreference(mChamberUnlocked);
         }
     }
 
@@ -1449,18 +1425,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             writeForceRtlOptions();
         } else if (preference == mWifiDisplayCertification) {
             writeWifiDisplayCertificationOptions();
-        } else if (preference == mChamber) {
-            if (Settings.Secure.getInt(getActivity().getContentResolver(),
-                    Settings.Secure.CHAMBER_OF_SECRETS, 0) == 0) {
-                Settings.Secure.putInt(getActivity().getContentResolver(),
-                        Settings.Secure.CHAMBER_OF_SECRETS, 1);
-                Toast.makeText(getActivity(),
-                        R.string.chamber_toast,
-                        Toast.LENGTH_LONG).show();
-                getPreferenceScreen().removePreference(mChamber);
-                addPreference(mChamberUnlocked);
-                mChamberUnlocked.setChecked(true);
-            }
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
@@ -1539,11 +1503,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             return true;
         } else if (preference == mAppProcessLimit) {
             writeAppProcessLimitOptions(newValue);
-            return true;
-        } else if (preference == mChamberUnlocked) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.CHAMBER_OF_SECRETS,
-                    (Boolean) newValue ? 1 : 0);
             return true;
         }
         return false;
